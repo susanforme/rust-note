81fe067e6e1252c1aee85aa2d3500c08cbb472c0
susanforme <rzc307853639@gmail.com>
2023-06-20 16:42:05 +0800

    ⬆️ 悬垂指针

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 fn main() {
-    let str = String::from("hello world");
-    let mut str2 = String::from("hello world");
-    let len = calculate_len(&str);
-    // 把对象的引用作为参数而不是所有权的转移,使用值
-    println!("len is {}! str is {}!", len, str);
-    change(&mut str2);
-    println!("str2 is {}!", str2);
+    let reference_to_nothing = dangle();
+}
+fn dangle() -> &String {
+    let s = String::from("hello");
+    &s
 }
 
-fn calculate_len(s: &String) -> usize {
-    s.len()
-}
-
-fn change(s: &mut String) {
-    // 不能修改借用的值
-    s.push_str("!");
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
 }
