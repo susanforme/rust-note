9ee70d8fae7a4c0cfce814ae990b954f7d9c2280
susanforme <rzc307853639@gmail.com>
2023-06-25 16:41:09 +0800

    所有权

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    let reference_to_nothing = dangle();
-}
-fn dangle() -> &String {
-    let s = String::from("hello");
-    &s
+    let s = String::from("hello world");
+    let word = first_word(&s[..]);
+    println!("the first word is {}!", word);
 }
 
-fn no_dangle() -> String {
-    let s = String::from("hello");
-    s
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    return &s[..];
 }
