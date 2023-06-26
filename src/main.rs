ed96c17dda2b27d48bad155bb3f5f7ec86c9f359
susanforme <rzc307853639@gmail.com>
2023-06-26 17:56:43 +0800

    完成所有权及开始结构体

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-fn main() {
-    let s = String::from("hello world");
-    let word = first_word(&s[..]);
-    println!("the first word is {}!", word);
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-    return &s[..];
+fn main() {
+    // 元组
+    let rect = (32, 10);
+    // 结构体
+    let rect2 = Rectangle {
+        width: 32,
+        height: 10,
+    };
+    println!("area is {}", area(rect));
+    println!("area is {}", area2(&rect2));
+}
+fn area(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
+}
+
+fn area2(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
 }
