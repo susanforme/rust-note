11dcf79e39c7fe1647426ef85e7b7d93e0a777cd
susanforme <rzc307853639@gmail.com>
2023-07-31 18:35:09 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let some_number = Some(5);
-    let some_char = Some('e');
+    let roll = 9;
+    match roll {
+        1 => {
+            println!("bingo your value is {}!", 1);
+        }
+        // 通配必须放最后,顺序匹配
+        other => move_player(other),
+        // 占位符 最后一个分支中明确地忽略了其他的值
+        // _ => (),
+    }
+}
 
-    let absent_number: Option<i32> = None;
+fn move_player(num: i32) {
+    println!("other value is {}!", num);
 }
