eb57b33e4447bdaea6206bf1513c766bd66f7465
susanforme <rzc307853639@gmail.com>
2023-08-01 22:10:42 +0800

    rust

---
+++ example/restaurant/src/front_of_house/serving.rs
@@ -0,0 +1,10 @@
+/*
+ * @Author: susanforme
+ * @Date: 2023-08-01 21:48:28
+ * @LastEditTime: 2023-08-01 22:03:36
+ * @FilePath: \restaurant\src\front_of_house\serving.rs
+ * @Description:
+ */
+pub fn take_order() {}
+fn serve_order() {}
+fn take_payment() {}
--- example/restaurant/src/lib.rs
+++ example/restaurant/src/lib.rs
@@ -1,1 +1,9 @@
-use std::io::{self, Write};
+pub use crate::front_of_house::{hosting, serving};
+
+// 解析front_of_house模块 不带花括号回去解析目录
+mod front_of_house;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    serving::take_order();
+}
