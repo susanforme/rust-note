0228dc2d77f084f12095cc8d423bbb698cf46b0c
susanforme <rzc307853639@gmail.com>
2023-08-03 17:43:34 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use std::cmp::max;
 use std::collections::HashMap;
 fn main() {
-    // 所有的键必须是相同类型，值也必须都是相同类型。
-    let mut scores = HashMap::new();
-    scores.insert(String::from("blue"), 10);
-    scores.insert(String::from("yellow"), 40);
+    let vec = vec![3, 3, 7, 4, 9, 9, 111, 1, 1, 1];
+    let mut map = HashMap::new();
+    let mut count = 0;
+    let mut key = 0;
 
-    // 访问map中的值
-    let team_name = String::from("blue");
-    // get方法返回Option<&V> copied获取Option<T> 接着调用 unwrap_or 在 score 中没有该键所对应的项时将其设置为零。
-    let score = scores.get(&team_name).copied().unwrap_or(0);
-    println!("the {} team score is {}!", team_name, score);
-
-    // 遍历 注意是随机顺序遍历
-    for (key, value) in &scores {
-        println!("{key}:{value}");
+    for i in 0..vec.len() {
+        let count = map.entry(vec[i]).or_insert(0);
+        *count += 1;
     }
+    for k in map.keys() {
+        let max_num = max(count, map.get(k).copied().unwrap_or(0));
+        if max_num != count {
+            key = *k;
+            count = max_num;
+        }
+    }
+    println!("众数是{},重复{}次!", &key, count);
 }
