36fbc94dda5a5c21292972655b8e4a5a4b853ae9
susanforme <rzc307853639@gmail.com>
2023-08-03 21:26:33 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use std::cmp::max;
-use std::collections::HashMap;
 fn main() {
-    let vec = vec![3, 3, 7, 4, 9, 9, 111, 1, 1, 1];
-    let mut map = HashMap::new();
-    let mut count = 0;
-    let mut key = 0;
+    let i = 100;
+    println!("{}", fib(i));
+}
 
-    for i in 0..vec.len() {
-        let count = map.entry(vec[i]).or_insert(0);
-        *count += 1;
+fn fib(i: i32) -> i32 {
+    if i == 1 || i == 2 {
+        return i;
     }
-    for k in map.keys() {
-        let max_num = max(count, map.get(k).copied().unwrap_or(0));
-        if max_num != count {
-            key = *k;
-            count = max_num;
-        }
-    }
-    println!("众数是{},重复{}次!", &key, count);
+    return fib(i - 1) + fib(i - 2);
 }
