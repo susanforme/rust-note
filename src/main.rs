101c1fd315e37aad9f264b2b8fa1dbd372fa39ef
susanforme <rzc307853639@gmail.com>
2023-08-08 18:30:23 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-use std::fmt::Display;
+use std::thread;
 
 fn main() {
-    let s: &'static str = "hello ";
-}
+    println!("不可变借用:");
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+    // 不可变借用
+    let only_borrows = || println!("From closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
 
-fn logest_with_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
-where
-    T: Display,
-{
-    println!("Announcement! {}", ann);
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+    println!("可变借用:");
+    let mut list_mut = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list_mut);
+    let mut borrows_mutably = || list_mut.push(7);
+    // cannot borrow `list_mut` as immutable because it is also borrowed as mutable
+    // println!("After calling closure: {:?}", list_mut);
+    borrows_mutably();
+    println!("After calling closure: {:?}", list_mut);
+
+    println!("所有权转移:");
+    let list_move = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list_move);
+    // 以便在一个新的线程而非主线程中打印 vector：
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
 }
