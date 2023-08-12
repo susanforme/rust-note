75dabc2a0060eb863da840cca1b6ba382547bcd1
susanforme <rzc307853639@gmail.com>
2023-08-12 21:13:05 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,27 @@
+use std::ops::Deref;
 fn main() {
-    filters_by_size();
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    // 底层调用等同于  *(y.deref())
+    // Rust 将 * 运算符替换为先调用 deref 方法再进行普通解引用的操作，
+    // 外边的普通解引用仍为必须的原因在于所有权。如果 deref 方法直接返回值而不是值的引用，其值（的所有权）将被移出 self。在这里以及大部分使用解引用运算符的情况下我们并不希望获取 MyBox<T> 内部值的所有权。
+    assert_eq!(5, *y);
 }
 
-#[derive(Debug, PartialEq)]
-struct Shoe {
-    size: u32,
-    style: String,
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
-    //  into_iter 来创建一个获取 vector 所有权的迭代器
-    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
-}
-
-fn filters_by_size() {
-    let shoes = vec![
-        Shoe {
-            size: 10,
-            style: String::from("sneaker"),
-        },
-        Shoe {
-            size: 13,
-            style: String::from("sandal"),
-        },
-        Shoe {
-            size: 10,
-            style: String::from("boot"),
-        },
-    ];
-    let in_my_size = shoes_in_size(shoes, 10);
-    assert_eq!(
-        in_my_size,
-        vec![
-            Shoe {
-                size: 10,
-                style: String::from("sneaker")
-            },
-            Shoe {
-                size: 10,
-                style: String::from("boot")
-            },
-        ]
-    );
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        // 访问元组结构体的第一个元素
+        &self.0
+    }
 }
