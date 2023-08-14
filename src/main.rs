937c80df056ecb91401c16b30afcc21d3ffe1a18
susanforme <rzc307853639@gmail.com>
2023-08-14 17:15:28 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,19 @@
-#![allow(unused)]
-
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
+use std::{thread, time::Duration};
 
 fn main() {
-    // 创建后 强引用为1 弱引用为0
-    let leaf = Rc::new(Node {
-        value: 3,
-        children: RefCell::new(vec![]),
-        parent: RefCell::new(Weak::new()),
+    // 这个程序的输出可能每次都略有不同
+    let handle = thread::spawn(|| {
+        // 只会打印到5,主线程结束
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread");
+            // sleep
+            thread::sleep(Duration::from_millis(1));
+        }
     });
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-    println!(
-        "leaf strong = {}, weak = {}",
-        Rc::strong_count(&leaf),
-        Rc::weak_count(&leaf),
-    );
-
-    {
-        let branch = Rc::new(Node {
-            value: 5,
-            children: RefCell::new(vec![Rc::clone(&leaf)]),
-            parent: RefCell::new(Weak::new()),
-        });
-        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-        println!(
-            "branch  strong_count is {} , weak_count is {}",
-            Rc::strong_count(&branch),
-            Rc::weak_count(&branch)
-        );
-        println!(
-            "leaf strong = {}, weak = {}",
-            Rc::strong_count(&leaf),
-            Rc::weak_count(&leaf),
-        );
+    for i in 1..5 {
+        println!("hi number {i} form the main thread!",);
+        thread::sleep(Duration::from_millis(1));
     }
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-    println!(
-        "leaf strong = {}, weak = {}",
-        Rc::strong_count(&leaf),
-        Rc::weak_count(&leaf),
-    );
-}
-#[derive(Debug)]
-struct Node {
-    value: i32,
-    children: RefCell<Vec<Rc<Node>>>,
-    // 父节点应该拥有其子节点：如果父节点被丢弃了，其子节点也应该被丢弃。然而子节点不应该拥有其父节点：如果丢弃子节点，其父节点应该依然存在
-    parent: RefCell<Weak<Node>>,
+    // join等待线程结束
+    handle.join().unwrap();
 }
