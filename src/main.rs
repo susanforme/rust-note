a6dcdca86928787ab2f0daec67ab0a0504c21381
susanforme <rzc307853639@gmail.com>
2023-08-14 16:32:59 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
-use std::{cell::RefCell, rc::Rc};
-#[derive(Debug)]
-enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
-    Nil,
-}
-fn main() {
-    let value = Rc::new(RefCell::new(5));
-    // bc 都可以引用 a
-    let a = Rc::new(List::Cons(Rc::clone(&value), Rc::new(List::Nil)));
-    let b = List::Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c = List::Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
-    // 这里使用了第五章讨论的自动解引用功能
-    *value.borrow_mut() += 10;
+#![allow(unused)]
 
-    // 都可以拥有修改后的值15
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
+
+fn main() {
+    // 创建后 强引用为1 弱引用为0
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new()),
+    });
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+            parent: RefCell::new(Weak::new()),
+        });
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        println!(
+            "branch  strong_count is {} , weak_count is {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+}
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+    // 父节点应该拥有其子节点：如果父节点被丢弃了，其子节点也应该被丢弃。然而子节点不应该拥有其父节点：如果丢弃子节点，其父节点应该依然存在
+    parent: RefCell<Weak<Node>>,
 }
