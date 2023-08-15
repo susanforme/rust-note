9ac71ae5f7e02ce6e7d487afc1577c41e6e22af0
susanforme <rzc307853639@gmail.com>
2023-08-15 17:21:44 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
-use std::{thread, time::Duration};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
-    // 这个程序的输出可能每次都略有不同
-    let handle = thread::spawn(|| {
-        // 只会打印到5,主线程结束
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread");
-            // sleep
-            thread::sleep(Duration::from_millis(1));
+    // mpsc 是 多个生产者，单个消费者（multiple producer, single consumer）的缩写
+    // tx 和 rx 通常作为 发送者（transmitter）和 接收者（receiver）的缩写
+    let (tx, rx) = mpsc::channel();
+    // 通过克隆发送者来创建多个生产者
+    let other_tx = tx.clone();
+    let start_str = String::from("welcome to channel!");
+    let handle = thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        tx.send(start_str).unwrap();
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+        // 编译失败所有权已转移
+        // println!("val is {}", val);
+    });
+    handle.join().unwrap();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
+            other_tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
     });
-    for i in 1..5 {
-        println!("hi number {i} form the main thread!",);
-        thread::sleep(Duration::from_millis(1));
+    // recv阻塞主进程直到接受到值
+    for received in rx {
+        println!("Got :{}", received);
     }
-    // join等待线程结束
-    handle.join().unwrap();
 }
