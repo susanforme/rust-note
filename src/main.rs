2cb06d2a55a366967419ec93cc0938aca07c4c9b
susanforme <rzc307853639@gmail.com>
2023-08-16 16:18:44 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,23 @@
-use std::{sync::mpsc, thread, time::Duration};
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
 fn main() {
-    // mpsc 是 多个生产者，单个消费者（multiple producer, single consumer）的缩写
-    // tx 和 rx 通常作为 发送者（transmitter）和 接收者（receiver）的缩写
-    let (tx, rx) = mpsc::channel();
-    // 通过克隆发送者来创建多个生产者
-    let other_tx = tx.clone();
-    let start_str = String::from("welcome to channel!");
-    let handle = thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        tx.send(start_str).unwrap();
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-        // 编译失败所有权已转移
-        // println!("val is {}", val);
-    });
-    handle.join().unwrap();
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-        for val in vals {
-            other_tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-    // recv阻塞主进程直到接受到值
-    for received in rx {
-        println!("Got :{}", received);
+    // 原子引用计数 Arc<T> 线程安全带有性能惩罚在必要时才为此买单
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            // counter是不可变的,但是提供了内部可变性
+            *num += 1;
+        });
+        handles.push(handle);
     }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
 }
