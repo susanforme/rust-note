5fcb8ffc4dd2faf0721a6105e65a0f2842c5de67
susanforme <rzc307853639@gmail.com>
2023-08-18 16:33:49 +0800

    rust

---
+++ hello_macro/hello_macro_derive/src/lib.rs
@@ -0,0 +1,29 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+// 指定名称HelloMacro就是 trait 名，
+// 相当于把struct传进来了
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    // 使用模版来替换
+    // 模版#name 会以name来替换
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
