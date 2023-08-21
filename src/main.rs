d4d209bd2b25406e5d13c5867c097664cdd149da
susanforme <rzc307853639@gmail.com>
2023-08-21 21:50:24 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+#![allow(unused)]
 use hello_macro::HelloMacro;
 use hello_macro_derive::HelloMacro;
+use rand::Rng;
 #[derive(HelloMacro)]
 struct Pancakes;
 fn main() {
+    let t = 1..=3;
+    const TWO_HOURS_IN_SECONDS: u32 = 60 * 60 * 2;
+    let other = 98_222;
+    let ary = [1, 2, 3, 4];
+    let str = "test";
+    rand::thread_rng().gen_range(t);
     Pancakes::hello_macro();
 }
