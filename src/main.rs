ed75b152024b726d0b8eb766f07c20a7d9b74460
susanforme <rzc307853639@gmail.com>
2023-08-23 18:38:57 +0800

    rust

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,17 @@ enum_str! {
 }
 
 fn main() {
+    // 不发起http请求，直接使用本地的json文件
+    // #[cfg(not(feature = "online-samples"))]
+    // json_typegen!("Point", r#"{ "x": 1, "y": 2 }"#);
+
+    // let str = r#"{ "x": 3, "y": 5 }"#;
+    // let mut p: Point = serde_json::from_str(str).unwrap();
+    // println!("deserialized = {:?}", p);
+    // p.x = 4;
+    // let serialized = serde_json::to_string(&p).unwrap();
+    // println!("serialized = {}", serialized);
+
     assert_eq!(Color::Red.name(), "Red");
     assert_eq!(PubColor::Red.name(), "Red");
 }
